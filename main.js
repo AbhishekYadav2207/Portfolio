7cf4aa734a9bc349aadf0bf6ed7b441ad3fd0fe4
Abhishek Yadav <130439265+AbhishekYadav2207@users.noreply.github.com>
2025-02-20 23:38:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Project data
 const projects = [
     {
-        title: 'No projeccts done...',
+        title: 'No projects done yet...',
         description: 'Looking forward to find some effective ideas to build Projects.',
         //image: 'https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?auto=format&fit=crop&w=600&q=80',
         tags: []
